6f97ebc3729f33cacc178b4e92b7cb8d5a14a120
journey4201 <52443953+journey4201@users.noreply.github.com>
2021-12-24 17:14:16 +0800

    add dynamic GET route

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 /*
 run server.js on terminal: node server.js
 end: CTRL+C
+specific access: http://localhost:8000/users/mark
 */ 
 const express = require('express'); // access to the express library by searching your node_modules for "express"
 const app = express(); // creates an instance of the express constructor
@@ -16,6 +17,15 @@ app.get('/users', function(req,res){
         message: 'successfully got users. Nice!',
         users: mockUserData
     })
+})
+
+app.get('/users/:id',function(req,res){
+	console.log(req.params.id)
+	res.json({
+		success: true,
+		message: 'got one user',
+		user: req.params.id
+	})
 })
 
 // listen for requests being made on localhost:8000
