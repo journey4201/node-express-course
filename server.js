aef04ac2ed5de55b3db46f915b372b59addb8900
journey4201 <52443953+journey4201@users.noreply.github.com>
2021-12-24 17:19:38 +0800

    add login POST route

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ specific access: http://localhost:8000/users/mark
 */ 
 const express = require('express'); // access to the express library by searching your node_modules for "express"
 const app = express(); // creates an instance of the express constructor
+// In order to make POST requests, first we need to include the "body-parser" library from our node_modules (included with express).
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
 
 // return some mock JSON data
 const mockUserData=[
@@ -26,6 +30,30 @@ app.get('/users/:id',function(req,res){
 		message: 'got one user',
 		user: req.params.id
 	})
+})
+
+// handle a POST request made to the 'login'
+app.post('/login',function(req,res){
+    // Typically passwords are encrypted using something like bcrypt before sending to database
+    const username=req.body.username;
+    const password=req.body.password;
+
+    // come from the database
+    const mockUsername="billyTheKid";
+    const mockPassword="superSecret";
+
+    if (username===mockUsername && password===mockPassword){
+         res.json({
+              success: true,
+              message: 'password and username match!',
+              token: 'encrypted token goes here'
+         })
+    } else {
+         res.json({
+              success: false,
+              message: 'password and username do not match'
+         })
+    }
 })
 
 // listen for requests being made on localhost:8000
